0d874e1fb783d66a145fd5d5ed1c5bad45c0d3dc
Shahzod010299 <shakhzodtoshboyev010299@gmail.com>
2023-04-19 03:58:12 +0500

    f c

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bloc_sample_1/CounterCubit.dart';
+import 'package:bloc_sample_1/counter_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() => runApp(CounterApp());
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => CounterCubit(),
+        child: CounterPage(),
+      ),
+    );
+  }
+}
